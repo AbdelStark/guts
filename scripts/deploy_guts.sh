2ce30ed68d4425f8615c42fb029e52c45aef1717
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2024-04-26 10:06:56 +0200

    🚧 test optimisation

--- scripts/deploy_guts.sh
+++ scripts/deploy_guts.sh
@@ -9,10 +9,11 @@ target_dir="./target/dev"
 contract_class_file="$target_dir/guts_Guts.contract_class.json"
 
 # Declare the contract and capture the command output
-command_output=$(starkli declare $contract_class_file --compiler-version=$compiler_version)
+#command_output=$(starkli declare $contract_class_file --compiler-version=$compiler_version)
 
 from_string="Class hash declared:"
-class_hash="${command_output#*$from_string}"
+#class_hash="${command_output#*$from_string}"
+class_hash="0x05e540e69c3a834dc920c45ecb0d57b902130ece8a9a5d8c96b9c30dec5d0733"
 
 echo "Deploying contract with class hash: $class_hash"
 
